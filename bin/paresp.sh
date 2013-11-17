4a6d5c4acca40854feb096e2fd42d62e09fd4021
AAFR <alffore@gmail.com>
2013-11-17 12:42:03 -0600

    Se incluye la opción para salidas mas procesables

---
+++ bin/paresp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for ((i=0; i<50000; i+=2))
+do
+
+./bprimo.exe $i 0
+
+done
--- src/main.c
+++ src/main.c
@@ -11,6 +11,9 @@
 
 int esPrimo(long n);
 long buscaDP(long n);
+
+int timp;
+
 /**
  * 
  * @param argc
@@ -19,37 +22,18 @@ long buscaDP(long n);
  */
 int main(int argc, char** argv) {
 
-    long n=atoi(*(argv+1));
+    long n = atoi(*(argv + 1));
     long cuenta;
-    
-    printf(" El número n: %ld es primo: %d\n",n,esPrimo(n));
-    
-    cuenta=buscaDP(n);
-    
-    printf("tantas parejas: %ld\n",cuenta);
-    
+
+    timp = 0 + atoi(*(argv + 2));
+
+    printf("# El número n: %ld es primo: %d\n", n, esPrimo(n));
+
+    cuenta = buscaDP(n);
+
+    printf("# tantas parejas:%ld:%ld\n", n,cuenta);
+
     return (EXIT_SUCCESS);
-}
-/**
- * 
- * @param n
- * @return 
- */
-int esPrimo(long n){
-    
-    
-    long i;
-    
-    for(i=2;i<n;i++){
-        
-  
-        if(n%i==0)return 0;
-        
-    }
-    
-    return 1;
-    
-    
 }
 
 /**
@@ -57,28 +41,54 @@ int esPrimo(long n){
  * @param n
  * @return 
  */
-long buscaDP(long n){
-    
+int esPrimo(long n) {
+
+
     long i;
-    
-    long r=n/2;
-    
-    long cuenta=0;
-    
-    long t=0;
-    
-    for (i=n-1;i>=r;i--){
-        
-        if(esPrimo(i)==1 && esPrimo(n-i)==1){
-            t=r*(r-1)*(r-1)+(r-n+i)*(r-n+i)*(3*r-2);
-            t/=2;
-            printf("%ld = %ld + %ld  :: r=%ld, a=%ld, t=%ld \n",n,i,n-i,r,r-n+i,t);
+
+    for (i = 2; i < n; i++) {
+
+
+        if (n % i == 0)return 0;
+
+    }
+
+    return 1;
+
+
+}
+
+/**
+ * 
+ * @param n
+ * @return 
+ */
+long buscaDP(long n) {
+
+    long i;
+
+    long r = n / 2;
+
+    long cuenta = 0;
+
+    long t = 0;
+
+    for (i = n - 1; i >= r; i--) {
+
+        if (esPrimo(i) == 1 && esPrimo(n - i) == 1) {
+            t = r * (r - 1)*(r - 1)+(r - n + i)*(r - n + i)*(3 * r - 2);
+            t /= 2;
+            if (timp > 0) {
+                printf("%ld = %ld + %ld  :: r=%ld, a=%ld, t=%ld \n", n, i, n - i, r, r - n + i, t);
+            } else {
+                printf("%ld %ld %ld %ld %ld %ld\n", n, i, n - i, r, r - n + i, t);
+            }
             cuenta++;
         }
-        
+
     }
-    
-    
+
+
     return cuenta;
-    
+
 }
